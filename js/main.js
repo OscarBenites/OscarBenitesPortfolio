1c0be3792783d8de1a9395a4826976a2a3e35f54
OscarBenites <you@example.com>
2022-10-20 22:56:35 -0500

    ThemeV1

--- js/main.js
+++ js/main.js
@@ -17,3 +17,18 @@ function scrollActive(){
 }
 
 window.addEventListener('scroll', scrollActive);
+
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const slider = document.getElementById('btheme');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+slider.addEventListener('click', ()  => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
